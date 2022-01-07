018c0144f02379e93ccdb816ecf0fdaf481a497e
Mao <maoxuepeng@gmail.com>
2022-01-07 09:38:47 +0800

    Update app.js

--- app.js
+++ app.js
@@ -3,7 +3,9 @@ const app = express()
 const port = 3000
 
 app.get('/', (req, res) => {
+  console.log(`Received request at: Date.now()`)
   res.send('Hello World! ' + new Date().toDateString())
+  
 })
 
 app.listen(port, () => {
